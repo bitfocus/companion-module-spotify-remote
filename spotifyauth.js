51c21fffabb17d5ca81e4d20027dfc1fffb5b82a
Julian Waller <git@julusian.co.uk>
2024-09-17 14:09:14 +0200

    wip: first draft of import

---
+++ spotifyauth.js
@@ -0,0 +1,39 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+$().ready(function() {
+   var urlParams = new URLSearchParams(window.location.search);
+   var codeParam = urlParams.get('code');
+   $("#approvalCode").html(codeParam);
+});
+
+}
+/*
+     FILE ARCHIVED ON 20:20:42 Nov 20, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 12:03:09 Sep 17, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1.231
+  exclusion.robots: 0.035
+  exclusion.robots.policy: 0.02
+  esindex: 0.013
+  cdx.remote: 6.574
+  LoadShardBlock: 59.727 (3)
+  PetaboxLoader3.datanode: 73.664 (4)
+  load_resource: 76.154
+  PetaboxLoader3.resolve: 32.35
+*/
